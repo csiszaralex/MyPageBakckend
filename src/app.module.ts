34e6ffb02f0a16b093b8dcbba29490eff2e2f214
csiszaralex <csiszaralex@gmail.com>
2023-02-11 15:58:07 +0100

    :loud_sound: Adds Logger for Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,39 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import appConfig from './config/app.config';
-import { PrismaModule } from 'nestjs-prisma';
+import { loggingMiddleware, PrismaModule, QueryInfo } from 'nestjs-prisma';
 import { UsersModule } from './users/users.module';
+import { AppConfig } from './config/app.config.interface';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true, load: [appConfig], expandVariables: true }),
-    PrismaModule.forRoot({ isGlobal: true }),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      useFactory: (configService: ConfigService<AppConfig>) => ({
+        prismaOptions: {
+          log: ['warn', 'error'],
+          errorFormat:
+            configService.get<string>('node_env') === 'development' ? 'pretty' : 'minimal',
+        },
+        prismaServiceOptions: {
+          explicitConnect: true,
+          middlewares: [
+            loggingMiddleware({
+              logger: new Logger('Prisma'),
+              logLevel: 'log',
+              logMessage(query: QueryInfo) {
+                return `${query.model}.${query.action}(${query.executionTime}ms)`;
+              },
+            }),
+          ],
+        },
+      }),
+    }),
     UsersModule,
   ],
   controllers: [AppController],
