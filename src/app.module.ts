c2e97f5a1e8f6bb3c235324498398b8820800f52
csiszaralex <csiszaralex@gmail.com>
2023-02-11 12:37:19 +0100

    :boom: Remove own prismaModule and replace to nestjs-prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import appConfig from './config/app.config';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from 'nestjs-prisma';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true, load: [appConfig], expandVariables: true }),
-    PrismaModule,
+    PrismaModule.forRoot({ isGlobal: true }),
     UsersModule,
   ],
   controllers: [AppController],
