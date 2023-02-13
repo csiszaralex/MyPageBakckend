1aa12f8662d846a3faa986d5d3d686538f29ce0c
csiszaralex <csiszaralex@gmail.com>
2023-02-13 23:44:30 +0100

    :white_check_mark: Stat testing in users module

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,19 +1,21 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { UsersController } from './users.controller';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
-  // let controller: UsersController;
+  let controller: UsersController;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     controllers: [UsersController],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, JwtModule],
+      controllers: [UsersController],
+    }).compile();
 
-  //   controller = module.get<UsersController>(UsersController);
-  // });
+    controller = module.get<UsersController>(UsersController);
+  });
 
-  it.todo('should be defined');
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined();
-  // });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 });
