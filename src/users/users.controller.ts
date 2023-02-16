42031261e1d0c50fca8608157c01e1b7e59eca18
csiszaralex <csiszaralex@gmail.com>
2023-02-16 21:54:53 +0100

    :art: Remove testing endpoints from users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,6 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { AppConfig } from 'src/config/app.config.interface';
 
 @Controller('users')
 @ApiTags('Users')
-export class UsersController {
-  constructor(
-    private readonly configService: ConfigService<AppConfig>,
-    private readonly jwtService: JwtService,
-  ) {}
-
-  @Get('token')
-  async token(@Query('jwt') jwt: string) {
-    const secret = this.configService.get('auth.jwt.accessToken.secret');
-    const data = this.jwtService.verify(jwt, { secret });
-    return { ...data, jwt };
-  }
-
-  @Get('test')
-  @UseGuards(AuthGuard('jwt'))
-  async test() {
-    return 'ok';
-  }
-}
+export class UsersController {}
